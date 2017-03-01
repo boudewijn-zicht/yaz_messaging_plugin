ac48498992f9e63f3fc686cf37363ef84fe85748
boudewijn <boudewijn@sniper.frayja.com>
2017-03-01 22:05:45 +0100

    1.0.0 Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name="yaz_messaging_plugin",
       packages=["yaz_messaging_plugin"],
-      version="1.0.0b1",
+      version="1.0.0",
       description="A symfony message translation plugin for YAZ",
       author="Boudewijn Schoon",
       author_email="boudewijn@zicht.nl",
