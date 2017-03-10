58dc6d251e6f98f3354d115e2e79627f11a5e801
boudewijn <boudewijn@sniper.frayja.com>
2017-03-10 12:17:13 +0100

    Rewrite tests to include check as well as cleanup

--- yaz_messaging_plugin/test/test_parsing.py
+++ yaz_messaging_plugin/test/test_parsing.py
@@ -1,8 +1,8 @@
 import yaz
-from .tester import TestCase
+from .tester import ClassTestCase
 
 
-class TestParsingWithColon(TestCase):
+class TestParsingWithColon(ClassTestCase):
     """
     The message should not contain an unquoted colon (:), this results in a parser error
     """
@@ -22,7 +22,8 @@ message: this line has a colon at : the end
         with self.assertRaisesRegex(yaz.Error, r"mapping values are not allowed here"):
             caller("cleanup")
 
-class TestParsingWithExclamationMark(TestCase):
+
+class TestParsingWithExclamationMark(ClassTestCase):
     """
     If the message starts with an exclamation mark, this results in a parser error
     """
@@ -43,7 +44,7 @@ message: !foo should be quoted
             caller("cleanup")
 
 
-class TestEmptyFile(TestCase):
+class TestEmptyFile(ClassTestCase):
     """
     Empty files should pass though the 'check' and not result in any changes
     """
@@ -60,60 +61,32 @@ class TestEmptyFile(TestCase):
         self.assertTrue(caller("cleanup"))
         self.assertEqual("", self.get_file_content("empty.nl.yml"))
 
-class TestTypesAsString(TestCase):
+
+class TestBooleanAsString(ClassTestCase):
     """
     YAML 1.1 supports type conversions for several types,
     in Symfony translations we want that disabled
 
-    Finally, we *will* output in valid YAML 1.1 format, hence all boolean values will
-    be properly quoted.
+    Finally, we *will* output in valid YAML 1.1 format, hence all
+    special type values will be properly quoted.
     """
     files = {
-        "boolean_type.nl.yml": """
+        "type.nl.yml": """
 false: False
 maybe.yes.or.no: Maybe
 no: No
 true: True
 yes: Yes
-""".lstrip(),
-
-        "float_type.nl.yml": """
-canonical: 6.8523015e+5
-exponentioal: 685.230_15e+03
-fixed: 685_230.15
-negative infinity: -.inf
-not a number: .NaN
-sexagesimal: 190:20:30.15
-    """.lstrip(),
-
-        "int_type.nl.yml": """
-binary: 0b1010_0111_0100_1010_1110
-canonical: 685230
-decimal: +685_230
-hexadecimal: 0x_0A_74_AE
-octal: 02472256
-sexagesimal: 190:20:30
-""".lstrip(),
-
-        "null_type.nl.yml": """
-canonical: ~
-empty:
-english: null
-~: null key
-""".lstrip(),
-
-        "timestamp_type.nl.yml": """
-canonical:        2001-12-15T02:59:43.1Z
-date (00:00:00Z): 2002-12-14
-no time zone (Z): 2001-12-15 2:59:43.10
-space separated:  2001-12-14 21:59:43.10 -5
-valid iso8601:    2001-12-14t21:59:43.10-05:00
 """.lstrip()
     }
 
-    def test_010_cleanup(self):
-        expected = {
-            "boolean_type.nl.yml": """
+    def test_010_check(self):
+        caller = self.get_caller()
+        with self.assertRaisesRegex(yaz.Error, r"changes detected in file"):
+            caller("check")
+
+    def test_020_cleanup(self):
+        expected = """
 'false': 'False'
 maybe:
     'yes':
@@ -122,43 +95,174 @@ maybe:
 'no': 'No'
 'true': 'True'
 'yes': 'Yes'
-""".lstrip(),
+""".lstrip()
 
-            "float_type.nl.yml": """
+        caller = self.get_caller()
+        caller("cleanup", "--changes", "overwrite")
+        self.assertEqual(expected, self.get_file_content("type.nl.yml"))
+
+    def test_030_check(self):
+        caller = self.get_caller()
+        self.assertTrue(caller("check"))
+
+class TestFloatAsString(ClassTestCase):
+    """
+    YAML 1.1 supports type conversions for several types,
+    in Symfony translations we want that disabled
+
+    Finally, we *will* output in valid YAML 1.1 format, hence all
+    special type values will be properly quoted.
+    """
+    files = {
+        "type.nl.yml": """
+canonical: 6.8523015e+5
+exponentioal: 685.230_15e+03
+fixed: 685_230.15
+negative infinity: -.inf
+not a number: .NaN
+sexagesimal: 190:20:30.15
+""".lstrip()
+    }
+
+    def test_010_check(self):
+        caller = self.get_caller()
+        with self.assertRaisesRegex(yaz.Error, r"changes detected in file"):
+            caller("check")
+
+    def test_020_cleanup(self):
+        expected = """
 canonical: '6.8523015e+5'
 exponentioal: '685.230_15e+03'
 fixed: '685_230.15'
 negative infinity: '-.inf'
 not a number: '.NaN'
 sexagesimal: '190:20:30.15'
-""".lstrip(),
+""".lstrip()
 
-            "int_type.nl.yml": """
+        caller = self.get_caller()
+        caller("cleanup", "--changes", "overwrite")
+        self.assertEqual(expected, self.get_file_content("type.nl.yml"))
+
+    def test_030_check(self):
+        caller = self.get_caller()
+        self.assertTrue(caller("check"))
+
+class TestIntAsString(ClassTestCase):
+    """
+    YAML 1.1 supports type conversions for several types,
+    in Symfony translations we want that disabled
+
+    Finally, we *will* output in valid YAML 1.1 format, hence all
+    special type values will be properly quoted.
+    """
+    files = {
+        "type.nl.yml": """
+binary: 0b1010_0111_0100_1010_1110
+canonical: 685230
+decimal: +685_230
+hexadecimal: 0x_0A_74_AE
+octal: 02472256
+sexagesimal: 190:20:30
+""".lstrip()
+    }
+
+    def test_010_check(self):
+        caller = self.get_caller()
+        with self.assertRaisesRegex(yaz.Error, r"changes detected in file"):
+            caller("check")
+
+    def test_020_cleanup(self):
+        expected = """
 binary: '0b1010_0111_0100_1010_1110'
 canonical: '685230'
 decimal: '+685_230'
 hexadecimal: '0x_0A_74_AE'
 octal: '02472256'
 sexagesimal: '190:20:30'
-""".lstrip(),
+""".lstrip()
 
-            "null_type.nl.yml": """
+        caller = self.get_caller()
+        caller("cleanup", "--changes", "overwrite")
+        self.assertEqual(expected, self.get_file_content("type.nl.yml"))
+
+    def test_030_check(self):
+        caller = self.get_caller()
+        self.assertTrue(caller("check"))
+
+class TestNullAsString(ClassTestCase):
+    """
+    YAML 1.1 supports type conversions for several types,
+    in Symfony translations we want that disabled
+
+    Finally, we *will* output in valid YAML 1.1 format, hence all
+    special type values will be properly quoted.
+    """
+    files = {
+        "type.nl.yml": """
+canonical: ~
+empty:
+english: null
+~: null key
+""".lstrip()
+    }
+
+    def test_010_check(self):
+        caller = self.get_caller()
+        with self.assertRaisesRegex(yaz.Error, r"changes detected in file"):
+            caller("check")
+
+    def test_020_cleanup(self):
+        expected = """
 canonical: '~'
 empty: ''
 english: 'null'
 '~': null key
-""".lstrip(),
+""".lstrip()
 
-            "timestamp_type.nl.yml": """
+        caller = self.get_caller()
+        caller("cleanup", "--changes", "overwrite")
+        self.assertEqual(expected, self.get_file_content("type.nl.yml"))
+
+    def test_030_check(self):
+        caller = self.get_caller()
+        self.assertTrue(caller("check"))
+
+class TestTimestampAsString(ClassTestCase):
+    """
+    YAML 1.1 supports type conversions for several types,
+    in Symfony translations we want that disabled
+
+    Finally, we *will* output in valid YAML 1.1 format, hence all
+    special type values will be properly quoted.
+    """
+    files = {
+        "type.nl.yml": """
+canonical:        2001-12-15T02:59:43.1Z
+date (00:00:00Z): 2002-12-14
+no time zone (Z): 2001-12-15 2:59:43.10
+space separated:  2001-12-14 21:59:43.10 -5
+valid iso8601:    2001-12-14t21:59:43.10-05:00
+""".lstrip()
+    }
+
+    def test_010_check(self):
+        caller = self.get_caller()
+        with self.assertRaisesRegex(yaz.Error, r"changes detected in file"):
+            caller("check")
+
+    def test_020_cleanup(self):
+        expected = """
 canonical: '2001-12-15T02:59:43.1Z'
 date (00:00:00Z): '2002-12-14'
 no time zone (Z): '2001-12-15 2:59:43.10'
 space separated: '2001-12-14 21:59:43.10 -5'
 valid iso8601: '2001-12-14t21:59:43.10-05:00'
 """.lstrip()
-        }
 
         caller = self.get_caller()
         caller("cleanup", "--changes", "overwrite")
-        for expected_file, expected_content in expected.items():
-            self.assertEqual(expected_content, self.get_file_content(expected_file))
+        self.assertEqual(expected, self.get_file_content("type.nl.yml"))
+
+    def test_030_check(self):
+        caller = self.get_caller()
+        self.assertTrue(caller("check"))
--- yaz_messaging_plugin/test/tester.py
+++ yaz_messaging_plugin/test/tester.py
@@ -9,22 +9,49 @@ class TestCase(yaz.TestCase):
     files = {}
 
     def setUp(self):
-        self.temporary_directory = tempfile.TemporaryDirectory()
+        self.__temporary_directory = tempfile.TemporaryDirectory()
 
         for filename, content in self.files.items():
-            with open(os.path.join(self.temporary_directory.name, filename), "w") as file:
+            with open(os.path.join(self.__temporary_directory.name, filename), "w") as file:
                 file.write(content)
 
         plugin = yaz.get_plugin_instance(Messaging)
-        plugin.dirs = [self.temporary_directory.name]
+        plugin.dirs = [self.__temporary_directory.name]
 
     def tearDown(self):
-        self.temporary_directory.cleanup()
+        self.__temporary_directory.cleanup()
 
     def get_caller(self, white_list=None):
         assert white_list is None, "Please let the tester handle this parameter"
         return super().get_caller([Messaging])
 
     def get_file_content(self, filename):
-        with open(os.path.join(self.temporary_directory.name, filename), "r") as file:
+        with open(os.path.join(self.__temporary_directory.name, filename), "r") as file:
+            return file.read()
+
+class ClassTestCase(yaz.TestCase):
+    files = {}
+    __temporary_directory = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.__temporary_directory = tempfile.TemporaryDirectory()
+
+        for filename, content in cls.files.items():
+            with open(os.path.join(cls.__temporary_directory.name, filename), "w") as file:
+                file.write(content)
+
+        plugin = yaz.get_plugin_instance(Messaging)
+        plugin.dirs = [cls.__temporary_directory.name]
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.__temporary_directory.cleanup()
+
+    def get_caller(self, white_list=None):
+        assert white_list is None, "Please let the tester handle this parameter"
+        return super().get_caller([Messaging])
+
+    def get_file_content(self, filename):
+        with open(os.path.join(self.__temporary_directory.name, filename), "r") as file:
             return file.read()
