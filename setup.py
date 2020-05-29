e7ceb6a8e5288761677dc4906d4a960a47ad3f9e
Boudewijn Schoon <boudewijn@zicht.nl>
2020-05-29 12:24:28 +0200

    Fix setup config

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import setuptools
 import sys
 
-with open("yaz_zichtgithub_plugin/version.py") as file:
+with open("yaz_messaging_plugin/version.py") as file:
     globals = {}
     exec(file.read(), globals)
     version = globals["__version__"]
